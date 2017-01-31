5bdca2ead3f9bbcebe811e2b040370b89565b5c4
Paul Petrov <paul@bargamut.ru>
2017-01-31 18:50:10 +0300

    Большая часть кода вынесена в библиотеку функций Исправлен баг продолжающейся установки даже при сбое сборки пакета

--- ajr.sh
+++ ajr.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
 
-#set -e
-
 # Билдер
 
 # Определение родной директории скрипта
 SCRIPT_DIR="$( cd "$( dirname $(readlink -e "${BASH_SOURCE[0]}") )" && pwd )"
 
 # подключаем служебные файлы
-source $SCRIPT_DIR/config.cfg
 source $SCRIPT_DIR/lib.sh
+source $SCRIPT_DIR/config.cfg
 
-# Даём шанс отступить
-echo -n "Установить пакет ${package}? (Y / N) "
+# Не случайно ли запустили?
+ask_YN "Установить пакет ${package}?" || exit $?
 
-read item
-
-case "$item" in
-  y|Y|д|Д) echo "Сами попросили..."
-      ;;
-  n|N|н|Н) echo "Отмена установки..."
-      exit 0
-      ;;
-  *) echo "Не понял ответа: завершаю работу"
-      exit 0
-      ;;
-esac
-  
 # Полезная работа
-dpkg-buildpackage -rfakeroot
+build
+BUILD_STATUS=$?
 
-check_success
-
-if [ -e $pkgpath ]
+if [ $BUILD_STATUS -eq 0 ]
 then
-  sudo dpkg -i $pkgpath
-  
-  check_success
+  install $pkgpath
 fi
 exit 0
+
+# TODO: реализовать механизм ключей, подобно этому: https://habrahabr.ru/post/158971/
--- lib.sh
+++ lib.sh
@@ -5,13 +5,72 @@ green=$(tput setf 2)
 reset=$(tput sgr0)
 toend=$(tput hpa $(tput cols))$(tput cub 6)
 
+# Функция импорта сторонних файлов
+# @since 0.2.0
+import() {
+  source "$1";
+  
+  if [ $? -ne 0 ]; then
+    echo "Ошибка: не найден подключаемый файл: $1" 1>&2
+    exit 254;
+  fi
+}
+
+# Спрашиваем для верности
+# @since 0.2.0
+ask_YN() {
+  local AMSURE
+  
+  if [ -n "$1" ]; then
+    read -n 1 -p "$1 (y / [ a ]): " AMSURE
+  else
+    read -n 1 AMSURE
+  fi
+  echo "" 1>&2
+
+  case "$AMSURE" in
+    y|Y|д|Д)
+      echo "Сами попросили..."
+      return 0
+      ;;
+    *)
+      echo "Отмена установки..."
+      return 255
+      ;;
+  esac
+}
+
 # Функция проверки статуса выполнения операции
+# @since 0.2.0
 check_success() {
-  if [ $? -eq 0 ]; then
+  if [ $1 -eq 0 ]; then
     echo -n "${green}${toend}[ OK ]"
   else
     echo -n "${red}${toend}[fail]"
   fi
   echo -n "${reset}"
   echo
+  
+  [ $1 -eq 0 ] && return 0 || return 255
+}
+
+# Собираем пакет
+# @since 0.2.0
+build() {
+  dpkg-buildpackage -rfakeroot
+
+  check_success $?
+  return $?
+}
+
+# Ставим пакет
+# @since 0.2.0
+install() {
+  if [ -e $1 ]
+  then
+    sudo dpkg -i $1
+    
+    check_success $?
+    return $?
+  fi
 }
