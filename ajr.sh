50bf10473d197e2795bbc69798dbcbc7cd27456a
Paul Petrov <paul@bargamut.ru>
2017-07-04 20:13:35 +0300

    * Добавлена возможность удаления deb-файла после установки пакета   * Добавлена возможность ответить разом на все вопросы (ответить Y)

--- ajr.sh
+++ ajr.sh
@@ -10,17 +10,43 @@ source $SCRIPT_DIR/lib.sh
 source $SCRIPT_DIR/config.cfg
 
 # Не случайно ли запустили?
-ask_YN "Собрать пакет ${package}?" || exit $?
+ask_YN "Собрать пакет ${package}?" "[ y ] - да,
+    [ Y ] - сборка, установка пакета, удаление deb-файла,
+    [ any key ] - прекратить работу."
+WAY=$?
+
+echo $WAY
+
+if [ $WAY -eq 255 ]
+then
+	exit $WAY
+fi
 
 # Полезная работа
 build
 BUILD_STATUS=$?
 
-if [ $BUILD_STATUS -eq 0 ]
+if [ $BUILD_STATUS -eq 0 ] || [ $BUILD_STATUS -eq 1 ]
 then
-  # Не случайно ли запустили?
-  ask_YN "Установить пакет ${package}?" || exit $?
-  install $pkgpath
+	if [ $WAY -ne 1 ]
+	then
+		# Хотим ли ставить?
+		ask_YN "Установить пакет ${package}?" "[ y ] - да,
+    [ any key ] - прекратить работу." || exit $?
+		WAY=$?
+	fi
+
+	install $pkgpath
+
+	if [ $WAY -ne 1 ]
+	then
+		# Надо ли удалить?
+		ask_YN "Удалить файлы сборки $pkgfiles?" "[ y ] - да,
+    [ any key ] - прекратить работу." || exit $?
+		WAY=$?
+	fi
+
+	deldeb $pkgfiles
 fi
 exit 0
 
--- lib.sh
+++ lib.sh
@@ -1,76 +1,96 @@
 #!/bin/bash
 
-red=$(tput setaf 4)
-green=$(tput setaf 2)
-reset=$(tput sgr0)
-toend=$(tput hpa $(tput cols))$(tput cub 6)
-
 # Функция импорта сторонних файлов
 # @since 0.2.0
 import() {
-  source "$1";
+	source "$1";
   
-  if [ $? -ne 0 ]; then
-    echo "Ошибка: не найден подключаемый файл: $1" 1>&2
-    exit 254;
-  fi
+	if [ $? -ne 0 ]; then
+		echo "Ошибка: не найден подключаемый файл: $1" 1>&2
+		exit 254;
+	fi
 }
 
 # Спрашиваем для верности
-# @since 0.2.0
+# y 						- согласие на отдельный вопрос
+# Y 						- согласие на все вопросы
+# любой символ 	- отмена операции, прекращение работы
+# @since 0.4.0
 ask_YN() {
-  local AMSURE
+	local AMSURE
   
-  if [ -n "$1" ]; then
-    read -n 1 -p "$1 (y / [ a ]): " AMSURE
-  else
-    read -n 1 AMSURE
-  fi
-  echo "" 1>&2
+	if [ -n "$1" ]; then
+		read -n 1 -p "$1
+    $2
+    Ваш выбор: " AMSURE
+	else
+		read -n 1 AMSURE
+	fi
+	echo "" 1>&2
 
-  case "$AMSURE" in
-    y|Y|д|Д)
-      echo "Сами попросили..."
-      return 0
-      ;;
-    *)
-      echo "Отмена установки..."
-      return 255
-      ;;
-  esac
+	case "$AMSURE" in
+		y|д)
+			echo "Сами попросили..."
+			return 0
+			;;
+		Y|Д)
+			echo "Вы согласились на всё сами..."
+			return 1
+			;;
+		*)
+			echo "Отмена..."
+			return 255
+			;;
+	esac
 }
 
 # Функция проверки статуса выполнения операции
 # @since 0.2.0
 check_success() {
-  if [ $1 -eq 0 ]; then
-    echo -n "${green}${toend}[ OK ]"
-  else
-    echo -n "${red}${toend}[fail]"
-  fi
-  echo -n "${reset}"
-  echo
+	local STATUS="[ OK ]"
+
+	if [ $1 -eq 0 ]; then
+		echo -n "${green}"
+	else
+		echo -n "${red}"
+		STATUS="[fail]"
+	fi
+
+	if [ -n "$2" ]; then
+		echo -n "$2"
+	fi
+
+	echo -n "${toend}$STATUS"
+	echo "${reset}"
   
-  [ $1 -eq 0 ] && return 0 || return 255
+	[ $1 -eq 0 ] && return 0 || return 255
 }
 
 # Собираем пакет
 # @since 0.2.0
 build() {
-  dpkg-buildpackage -rfakeroot
+	dpkg-buildpackage -rfakeroot
 
-  check_success $?
-  return $?
+	check_success $? "Сборка..."
+	return $?
 }
 
 # Ставим пакет
 # @since 0.2.0
 install() {
-  if [ -e $1 ]
-  then
-    sudo dpkg -i $1
-    
-    check_success $?
-    return $?
-  fi
+	if [ -e $1 ]; then
+		sudo dpkg -i $1
+
+		check_success $? "Установка..."
+		return $?
+	fi
+}
+
+# Удаляем сбилдованный deb-файл
+# @since 0.4.0
+deldeb() {
+	rm $*
+
+	check_success $? "Удаление файлов сборки..."
+	return $?
 }
